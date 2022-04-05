49aa4418a111f47cbb80bf7fd1e6e31dc3104195
ericxspace <75740269+ericxspace@users.noreply.github.com>
2022-04-05 08:44:07 +0700

    baitap

---
+++ 05-04/bai tap /[bt] Do you love me /dylm.js
@@ -0,0 +1,12 @@
+// Nut Yes
+function hienthi(){
+    alert("<3")
+}
+// Nut No
+
+function nhay() {
+    let x=Math.floor(Math.random()*window.innerWidth);
+    let y=Math.floor(Math.random()*window.innerWidth);
+    document.getElementById("no").style.left = x + "px";
+    document.getElementById("no").style.top = y + "px";
+}
